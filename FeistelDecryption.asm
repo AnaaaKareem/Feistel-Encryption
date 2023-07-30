3bd6da5c83fdcfa6f0ce9a0563577b435fc3bb72
Kareem Khater <118448251+AnaaaKareem@users.noreply.github.com>
2023-07-30 15:00:16 +0000

    Added the encryption and key rotation from FeistelEncryption

--- FeistelDecryption.asm
+++ FeistelDecryption.asm
@@ -133,3 +133,135 @@ M=M-1
 @LASTKEYLOOP
 0;JMP
 (LASTKEYDONE)
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// ¬Ki //
+@R1
+D=!M
+@255
+D=D&A
+@R1
+M=D
+
+// Create an encryption counter for the first 4 rounds
+@4
+D=A
+// Store the value three in the variable ENCRYPTCOUNT
+@ENCRYPTCOUNT
+M=D
+
+//// Start the encryption process ////
+
+(ENCRYPTLOOP)
+@ENCRYPTCOUNT
+D=M
+@FINISHED
+D;JEQ
+
+/// Store Li ///
+// Load the Li using the Address register to the Data register to store it
+@R3
+D=M
+// Store Li in the Data register to the Memory register to be used to calculate the Ri+1
+@Li
+M=D
+
+/// Li+1 = Ri ///
+// Load Ri 
+@R2
+D=M
+// Store it to create Li+1
+@R3
+M=D
+
+// F(Ri, Ki) = Ri ⊕ ¬Ki //
+
+@R2
+D=M
+
+@R1
+D=D&M
+
+@buffer
+M=D
+
+@R2
+D=M
+
+@R1
+D=D|M
+
+@buffer1
+M=D
+
+@buffer
+D=!M
+
+@buffer1
+D=D&M
+@FUNCTION
+M=D
+
+// Ri+1 = Li ⊕ F(Ri, Ki) //
+
+@Li
+D=M
+
+@FUNCTION
+D=D&M
+
+@buffer
+M=D
+
+@Li
+D=M
+
+@FUNCTION
+D=D|M
+
+@buffer1
+M=D
+
+@buffer
+D=!M
+
+@buffer1
+D=D&M
+@R2
+M=D
+
+//// Rotate Key ////
+
+// Check if the MSB is 1
+@R1
+D=M
+@128
+D=D&A
+@NOROTATION
+D;JEQ
+
+// If true rotate bit
+@R1
+D=M+1
+D=M+D
+@255
+D=D&A
+@R1
+M=D
+@SKIP
+0;JMP
+
+(NOROTATION)
+@R1
+D=M
+M=M+D
+
+(SKIP)
+@ENCRYPTCOUNT
+M=M-1
+@ENCRYPTLOOP
+0;JMP
+(FINISHED)
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
